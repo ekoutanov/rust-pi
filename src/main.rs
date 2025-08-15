b81ef50805d4ddb954b237f8d7c61aedaf477c21
Emil Koutanov <ekoutanov@gmail.com>
2025-08-16 07:35:47 +1000

    Subsampling

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,50 @@
 use std::time::Instant;
-use tinyrand::{Rand, StdRand};
+use tinyrand::{Rand, Seeded, StdRand};
 
 fn main() {
     let start_time = Instant::now();
-    let pi = gen_pi();
-    let elapsed = start_time.elapsed();
-    println!("pi = {pi}, took {:.1} s", elapsed.as_millis() as f64 / 1000.0);
+    const SAMPLES: usize = 1_000;
+    const ITERS: usize = 1_000_000;
+    let mut samples = (0..SAMPLES).map(|sample| {
+        let mut rand = StdRand::seed(sample as u64);
+        gen_pi(ITERS, &mut rand)
+        
+    }).collect::<Vec<_>>();
+    samples.sort_unstable_by(|a, b| a.total_cmp(b));
+    
+    let min = quantile(&samples, 0.0);
+    let p025 = quantile(&samples, 0.025);
+    let p05 = quantile(&samples, 0.05);
+    let median = quantile(&samples, 0.5);
+    let p95 = quantile(&samples, 0.95);
+    let p975 = quantile(&samples, 0.975);
+    let max = quantile(&samples, 1.0);
+    let mean = samples.iter().sum::<f64>() / samples.len() as f64;
+    
+    println!("min: {min:.9}, median: {median:.9}, max: {max:.9}");
+    println!("mean: {mean:.9}");
+    println!("ci 90: [{p05:.9}, {p95:.9}]");
+    println!("ci 95: [{p025:.9}, {p975:.9}]");
+    println!("took {:.1} s", start_time.elapsed().as_millis() as f64 / 1000.0);
+    println!("true value: {}", std::f64::consts::PI);
+    
 }
 
-// fn gen_pi() -> f64 {
-//     let dim = 10000_u64;
-//     let mut count = 0_u64;
-//     for i in 0..=dim {
-//         let x = i as f64 / dim as f64;
-//         for j in 0..=dim {
-//             let y = j as f64 / dim as f64;
-//             if (x.powi(2) + y.powi(2)).sqrt() <= 1.0 {
-//                 count += 1;
-//             }
-//         }
-//     }
-//     let area = count as f64 / dim.pow(2) as f64;
-//     area * 4.0
-// }
-
-// fn gen_pi() -> f64 {
-//     let dim = 10000_u64;
-//     let mut count = 0_u64;
-//     for i in 0..=dim {
-//         for j in 0..=dim {
-//             if ((i.pow(2) + j.pow(2)) as f64).sqrt() <= dim as f64 {
-//                 count += 1;
-//             }
-//         }
-//     }
-//     let area = count as f64 / dim.pow(2) as f64;
-//     area * 4.0
-// }
-
-fn gen_pi() -> f64 {
-    let mut rand = StdRand::default();
-
+fn gen_pi(iters: usize, rand: &mut impl Rand) -> f64 {
     let mut count = 0_u64;
-    const ITERS: u64 = 1_000_000_000;
-    for _ in 0..ITERS {
+    for _ in 0..iters {
         let x = rand.next_u64() as f64 / u64::MAX as f64;
         let y = rand.next_u64() as f64 / u64::MAX as f64;
         if x * x + y * y <= 1.0 {
             count += 1;
         }
     }
-    let area = count as f64 / ITERS as f64;
+    let area = count as f64 / iters as f64;
     area * 4.0
+}
+
+pub fn quantile<T>(ordered: &[T], quantile: f64) -> &T {
+    let index = (quantile * ordered.len() as f64) as usize;
+    let index = if index == 0 { 0 } else { index - 1 };
+    &ordered[index]
 }
