68ea438ce6cc4e50235233891fac8175740579c0
Emil Koutanov <ekoutanov@gmail.com>
2025-08-18 09:05:11 +1000

    Unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,31 @@ impl Display for ConfidenceInterval {
         let mean = (self.lower + self.upper) / 2.0;
         let span = self.upper - mean;
         match f.precision() {
-            None => write!(f, "[{}, {}], \u{0078}\u{0304} = {mean} ± {span:}", self.lower, self.upper),
-            Some(precision) => write!(f, "[{:.precision$}, {:.precision$}], \u{0078}\u{0304} = {mean:.precision$} ± {span:.precision$}", self.lower, self.upper)
+            None => write!(f, "[{}, {}], x\u{0304} = {mean} ± {span:}", self.lower, self.upper),
+            Some(precision) => write!(f, "[{:.precision$}, {:.precision$}], x\u{0304} = {mean:.precision$} ± {span:.precision$}", self.lower, self.upper)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ConfidenceInterval;
+
+    #[test]
+    fn display() {
+        let ci = ConfidenceInterval {
+            lower: 12.35,
+            upper: 34.85,
+        };
+        assert_eq!("[12.35, 34.85], x\u{0304} = 23.6 ± 11.25", format!("{ci}"));
+    }
+
+    #[test]
+    fn display_with_precision() {
+        let ci = ConfidenceInterval {
+            lower: 12.35,
+            upper: 34.85,
+        };
+        assert_eq!("[12.3, 34.9], x\u{0304} = 23.6 ± 11.2", format!("{ci:.1}"));
     }
 }
